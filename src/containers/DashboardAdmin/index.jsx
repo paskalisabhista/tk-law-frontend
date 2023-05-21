b3b7b6e610a0aa0cc44d440028800b5a383ccae6
Ahmad Rafi Wirana <rafi.wirana@rey.id>
2023-05-22 00:19:31 +0700

    feat: init dashboard design

--- src/containers/DashboardAdmin/index.jsx
+++ src/containers/DashboardAdmin/index.jsx
@@ -1,7 +1,33 @@
-export default function DashboardAdminContainer(){
+export default function DashboardAdminContainer() {
     return (
-        <div>
-            Dashboard page
+        <div className="flex h-screen">
+            <div className="p-3 space-y-2 overflow-y-scroll bg-stone-300 w-64 h-full rounded">
+                <h2 className="text-2xl font-semibold text-gray-800">Admin Dashboard</h2>
+                <nav>
+                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
+                        Dashboard
+                    </a>
+                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
+                        Customers Users
+                    </a>
+                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
+                        Kitchen Users
+                    </a>
+                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
+                        Courier Users
+                    </a>
+                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
+                        Admin Users
+                    </a>
+                </nav>
+            </div>
+            <div className="flex-grow p-6">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-4">Welcome to the Admin Dashboard</h2>
+                <div className="bg-stone-300 p-4 rounded-lg shadow">
+                    Main content
+                </div>
+            </div>
         </div>
     );
-}
+};
+
