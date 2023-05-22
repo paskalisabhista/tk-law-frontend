4bb0037f1032f9a93a7c03776de67bcdc69095d8
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-05-22 13:17:45 +0700

    feat: handle role on dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+import useLogin from "@/src/utils/useLogin";
 import DashboardAdminContainer from "@/src/containers/DashboardAdmin";
 
 export async function getStaticProps() {
-    return { props: { title: "Menu" , withNavbar: true} };
+  return { props: { title: "Menu", withNavbar: true } };
 }
 
 export default function Menu() {
-    return (
-        <>
-            <DashboardAdminContainer/>
-        </>
-    );
+  const { replace } = useRouter();
+  const { detail } = useLogin();
+
+  const [role, setRole] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const user = await detail();
+        if (user.role !== "admin") replace("/");
+        setRole(user.role);
+      } catch (err) {
+        process.env.NODE_ENV === "development" && console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [detail, replace]);
+
+  if (loading || role !== "admin") return null;
+  return <DashboardAdminContainer />;
 }
