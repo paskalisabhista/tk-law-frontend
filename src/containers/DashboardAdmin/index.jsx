3a155f9159c0ed7b180be450122a6759c780cd5c
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-05-22 13:02:31 +0700

    feat: setup tab in admin dashboard

--- src/containers/DashboardAdmin/index.jsx
+++ src/containers/DashboardAdmin/index.jsx
@@ -1,33 +1,36 @@
-export default function DashboardAdminContainer() {
-    return (
-        <div className="flex h-screen">
-            <div className="p-3 space-y-2 overflow-y-scroll bg-stone-300 w-64 h-full rounded">
-                <h2 className="text-2xl font-semibold text-gray-800">Admin Dashboard</h2>
-                <nav>
-                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
-                        Dashboard
-                    </a>
-                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
-                        Customers Users
-                    </a>
-                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
-                        Kitchen Users
-                    </a>
-                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
-                        Courier Users
-                    </a>
-                    <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-400 hover:text-white">
-                        Admin Users
-                    </a>
-                </nav>
-            </div>
-            <div className="flex-grow p-6">
-                <h2 className="text-2xl font-semibold text-gray-800 mb-4">Welcome to the Admin Dashboard</h2>
-                <div className="bg-stone-300 p-4 rounded-lg shadow">
-                    Main content
-                </div>
-            </div>
-        </div>
-    );
-};
+import { useState } from "react";
 
+export default function DashboardAdminContainer() {
+  const [tab, setTab] = useState("menu");
+
+  return (
+    <div className="flex h-screen">
+      <div className="p-3 space-y-2 overflow-y-scroll bg-stone-300 w-64 h-full rounded">
+        <h2 className="text-2xl font-semibold text-gray-800">
+          Admin Dashboard
+        </h2>
+        <nav className="flex items-center">
+          {["menu", "coupon"].map((tab) => (
+            <button
+              key={tab}
+              type="button"
+              className="px-2 py-1 text-[#F4ECE1] bg-[#2F2F2F] rounded-xl drop-shadow-2xl capitalize"
+              onClick={() => setTab(tab)}
+            >
+              {tab}
+            </button>
+          ))}
+        </nav>
+      </div>
+
+      <div className="flex-grow p-6">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          Welcome to the Admin Dashboard
+        </h2>
+        <div className="bg-stone-300 p-4 rounded-lg shadow">
+          {tab === "menu" ? <></> : tab === "coupon" ? <></> : null}
+        </div>
+      </div>
+    </div>
+  );
+}
